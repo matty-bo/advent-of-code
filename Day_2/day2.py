21dac443f7b5eaef1f2ca0783764cc947ab23773
Phals3 <mateuszborowiecki.ik@gmail.com>
2019-12-11 01:10:25 +0100

    removed blank spaces and shortened

--- Day_2/day2.py
+++ Day_2/day2.py
@@ -1,49 +1,29 @@
-def answer():
-    filename = "day2_data.txt"
-    clean_data = load_data(filename)
-    data = clean_data.copy()
-    intcode_computer(data)
-    print("Task 1:", data[0])
-    print("Task 2:", find_pairs(clean_data))
-
-
-def intcode_computer(numbers):
-    for i in range(0, len(numbers), 4):
-        op_code = numbers[i]
+def computer(mem):
+    for i in range(0, len(mem), 4):
+        op_code = mem[i]
         if op_code == 1:
-            numbers[numbers[i + 3]] = numbers[numbers[i + 1]] + numbers[numbers[i + 2]]
+            mem[mem[i + 3]] = mem[mem[i + 1]] + mem[mem[i + 2]]
         elif op_code == 2:
-            numbers[numbers[i + 3]] = numbers[numbers[i + 1]] * numbers[numbers[i + 2]]
+            mem[mem[i + 3]] = mem[mem[i + 1]] * mem[mem[i + 2]]
         elif op_code == 99:
-            return
-        else:
-            # print("Wrong operation code!")
-            return
-    return numbers
+            return mem[0]
 
+def calculate_output(data, noun, verb):
+    data[1], data[2] = noun, verb
+    return computer(data)
+
+def find_pairs(mem):
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            if calculate_output(mem.copy(), noun, verb) == 19690720:
+                return 100*noun+verb
 
 def load_data(filename):
     with open(filename) as file:
-        data = file.read()
-        data = data.split(sep=',')
+        data = file.read().split(',')
         data = list(map(int, data))
     return data
 
-
-def calculate_output(data, noun, verb):
-    data[1] = noun
-    data[2] = verb
-    intcode_computer(data)
-    return data[0]
-
-
-def find_pairs(clear_data):
-
-    for noun in range(0, 100):
-        for verb in range(0, 100):
-            data = clear_data.copy()
-            if calculate_output(data, noun, verb) == 19690720:
-                return 100*noun+verb
-
-
-answer()
+filename = "day2_data.txt"
+print("Task 1:", computer(load_data(filename)))
+print("Task 2:", find_pairs(load_data(filename)))
