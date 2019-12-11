fe7970c3105a72566ca0edf7230fd4a2462b00c7
Phals3 <mateuszborowiecki.ik@gmail.com>
2019-12-11 19:29:30 +0100

    minimalised code

--- Day_5/day5.py
+++ Day_5/day5.py
@@ -1,64 +1,43 @@
-def handle_arguments(mem, ip, instruction, arg_amount):
-    arg_values = []
-    x = mem[ip + 1]
-    if instruction[-1] == "0":
-        x = mem[x]
-    arg_values.append(x)
+def ga(modes, ip, shift):
+    if modes[-shift] == "0":
+        return memory[ip + shift]
+    return ip + shift
 
-    if arg_amount == 2:
-        y = mem[ip + 2]
-        if instruction[-2] == "0":
-            y = mem[y]
-        arg_values.append(y)
-    return arg_values
-
-def computer(mem, user_input, ip=0):
-    while ip < len(mem):
-        op_code = int(str(mem[ip])[-2:])
-        instruction = str(mem[ip])[:-2]
-        n = 3-len(instruction)
-        instruction = instruction.zfill(n + len(instruction))
-        args_list = handle_arguments(mem, ip, instruction, 2)
-
-        if op_code == 4:
-            arg1, _ = args_list
-        else:    # 2,3 arguments
-            arg1, arg2 = args_list
-
+def computer(memory, user_input, ip=0):
+    while ip < len(memory):
+        op_code = memory[ip] % 100
+        modes = str(memory[ip])[:-2].zfill(3)
         if op_code == 1:
-            mem[mem[ip + 3]] = arg1 + arg2
-            ip += 4
+            memory[memory[ip + 3]] = memory[ga(modes, ip, 1)] + memory[ga(modes, ip, 2)]
         elif op_code == 2:
-            mem[mem[ip + 3]] = arg1 * arg2
-            ip += 4
+            memory[memory[ip + 3]] = memory[ga(modes, ip, 1)] * memory[ga(modes, ip, 2)]
         elif op_code == 3:
-            mem[mem[ip + 1]] = user_input
-            ip += 2
+            memory[memory[ip + 1]] = user_input
         elif op_code == 4:
-            print(arg1)
-            ip += 2
+            print(memory[ga(modes, ip, 1)])
         elif op_code == 5:
-            if arg1 != 0:
-                ip = arg2
+            if memory[ga(modes, ip, 1)] != 0:
+                ip = memory[ga(modes, ip, 2)]
             else:
                 ip += 3
         elif op_code == 6:
-            if arg1 == 0:
-                ip = arg2
+            if memory[ga(modes, ip, 1)] == 0:
+                ip = memory[ga(modes, ip, 2)]
             else:
                 ip += 3
         elif op_code == 7:
-            mem[mem[ip + 3]] = arg1 < arg2
-            ip += 4
+            memory[memory[ip + 3]] = memory[ga(modes, ip, 1)] < memory[ga(modes, ip, 2)]
         elif op_code == 8:
-            mem[mem[ip + 3]] = arg1 == arg2
-            ip += 4
+            memory[memory[ip + 3]] = memory[ga(modes, ip, 1)] == memory[ga(modes, ip, 2)]
         else:
             assert op_code == 99
             return
+        if op_code in [1, 2, 7, 8]:
+            ip += 4
+        elif op_code in [3, 4]:
+            ip += 2
 
 filename = "day5_data.txt"
 with open(filename) as file:
-    data = list(map(int, file.read().split(',')))
-print("Diagnostic code: ", end='')
-computer(data, 5)
+    memory = list(map(int, file.read().split(',')))
+computer(memory, 5)
