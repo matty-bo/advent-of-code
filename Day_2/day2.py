b4d306ea3dafac2d5b73f2e6d9248b4466527af2
Phals3 <mateuszborowiecki.ik@gmail.com>
2019-12-11 12:54:02 +0100

    cosmetic changes

--- Day_2/day2.py
+++ Day_2/day2.py
@@ -8,22 +8,16 @@ def computer(mem):
         elif op_code == 99:
             return mem[0]
 
-def calculate_output(data, noun, verb):
-    data[1], data[2] = noun, verb
-    return computer(data)
-
 def find_inputs(mem):
     for noun in range(0, 100):
         for verb in range(0, 100):
-            if calculate_output(mem.copy(), noun, verb) == 19690720:
+            data = mem.copy()
+            data[1], data[2] = noun,  verb
+            if computer(data) == 19690720:
                 return 100*noun+verb
 
-def load_data(filename):
-    with open(filename) as file:
-        data = file.read().split(',')
-        data = list(map(int, data))
-    return data
-
 filename = "day2_data.txt"
-print("Task 1:", computer(load_data(filename)))
-print("Task 2:", find_inputs(load_data(filename)))
+with open(filename) as file:
+    data = list(map(int, file.read().split(',')))
+print("Task 1:", computer(data.copy()))
+print("Task 2:", find_inputs(data))
