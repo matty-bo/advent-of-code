e66bbb08b9a36af33cec60a06e9a395d39eac116
Phals3 <mateuszborowiecki.ik@gmail.com>
2020-02-23 23:29:39 +0100

    Day 9

---
+++ Day_9/day9.py
@@ -0,0 +1,30 @@
+from itertools import permutations
+
+
+def distance(way):
+    dist = 0
+    for i in range(len(way)-1):
+        dist += int(data.get((way[i], way[i+1]), None))
+    return dist
+
+
+filename = 'day9_data.txt'
+data = {}
+with open(filename) as file:
+    for line in file:
+        row = line.strip().replace(' to ', ' ').replace(' = ', ',')
+        c, dist = row.split(',')
+        (c1, c2) = c.split()
+        data[(c1, c2)] = dist
+        data[(c2, c1)] = dist
+
+cities = set()
+for line in data:
+    cities.add(line[0])
+    cities.add(line[1])
+cities = [c for c in cities]
+ways = permutations(cities)
+distances = [distance(perm) for perm in ways]
+ans1, ans2 = min(distances), max(distances)
+print("Answer 1:", ans1)
+print("Answer 2:", ans2)
