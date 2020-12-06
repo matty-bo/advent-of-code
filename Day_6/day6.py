e11a8881772949ca260d057319e7776a82bd7a9a
Phals3 <phalse3@gmail.com>
2020-12-06 11:33:19 +0100

    day6 solutions

--- Day_6/day6.py
+++ Day_6/day6.py
@@ -5,7 +5,7 @@ with open(filename) as file:
 ans1 = sum(len({char for char in s if char != '\n'}) for s in data)
 print('Answer 1:', ans1)
 
-group_sets = [[{char for char in answers} for answers in g.split()] for g in data]
-everyone = [len(g[0].intersection(*g[1:])) for g in group_sets]
+group_sets = [[{ans for ans in answers} for answers in group.split()] for group in data]
+everyone = [len(answers[0].intersection(*answers[1:])) for answers in group_sets]
 ans2 = sum(everyone)
 print('Answer 2:', ans2)
