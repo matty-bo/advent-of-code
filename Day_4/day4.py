1cf82b0d110147d6d916e6bfa460f73ad6a0c047
Phals3 <phalse3@gmail.com>
2020-12-08 11:15:06 +0100

    fixed regex for hgt and replaced multiple if statements with regex patterns dictionary

--- Day_4/day4.py
+++ Day_4/day4.py
@@ -1,6 +1,5 @@
 import re
 
-
 filename = 'input4.txt'
 with open(filename) as file:
     data = file.read().split('\n\n')
@@ -10,6 +9,15 @@ passports = [{f.split(':')[0]: f.split(':')[1] for f in p} for p in data]
 req_fields = sorted(['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'])
 
 ans1, ans2 = 0, 0
+regex_patterns = {
+    'byr': r'19[2-9][0-9]|200[0-2]',
+    'iyr': r'20(1[0-9]|20)',
+    'eyr': r'20(2[0-9]|30)',
+    'hgt': r'(1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in',
+    'hcl': r'#([0-9a-f]){6}',
+    'ecl': r'amb|blu|brn|gry|grn|hzl|oth',
+    'pid': r'\d{9}'
+}
 for p in passports:
     fields = list(p.keys())
     if 'cid' in fields:
@@ -18,21 +26,8 @@ for p in passports:
     ans1 += valid
     valid2 = True
     if valid:
-        if not re.fullmatch(r'19[2-9][0-9]|200[0-2]', p['byr']):
-            valid2 = False
-        if not re.fullmatch(r'20(1[0-9]|20)', p['iyr']):
-            valid2 = False
-        if not re.fullmatch(r'20(2[0-9]|30)', p['eyr']):
-            valid2 = False
-        if not re.fullmatch(r'(1[5-9][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in', p['hgt']):
-            valid2 = False
-        if not re.fullmatch(r'#([0-9a-f]){6}', p['hcl']):
-            valid2 = False
-        if not re.fullmatch(r'amb|blu|brn|gry|grn|hzl|oth', p['ecl']):
-            valid2 = False
-        if not re.fullmatch(r'\d{9}', p['pid']):
-            valid2 = False
-        ans2 += valid2
+        fields_match = [re.fullmatch(regex_patterns[field], p[field]) is not None for field in fields]
+        ans2 += all(fields_match)
 
 print('Answer 1:', ans1)
 print('Answer 2:', ans2)
